68a5ab128d412d4b6705678ae20c5a87a192a9b9
zuoqiang <zuoqiang@bsfit.com.cn>
2017-05-18 22:27:08 +0800

    test

---
+++ src/main/java/com/example/SpringBoot/App.java
@@ -0,0 +1,35 @@
+package com.example.SpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * Hello world!
+ *
+ */
+
+@RestController
+@EnableAutoConfiguration
+public class App 
+{	
+	@RequestMapping("/h")
+    public String home() {
+      return "Hello world";
+    }
+     
+	@RequestMapping("/")
+	public String test(ModelMap map){
+        map.addAttribute("host", "My test");
+		return "index";
+	}
+	
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        SpringApplication.run(App.class, args);
+    }
+}
